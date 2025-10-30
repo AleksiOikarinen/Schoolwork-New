21fbe0e1c4be2d5e056b6b842fb335e4bfd603c3
AleksiOikarinen <aleksi23007@student.hamk.fi>
2025-10-30 13:59:03 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -11,36 +11,31 @@ public class Test {
         cars.add("BMW");
         cars.add("Renault");
 
-        // Print the original ArrayList (line by line)
         for (String car : cars) {
             System.out.println(car);
         }
 
-        // Insert Ford after Kia
         cars.add(1, "Ford");
 
-        // Delete Tesla
         cars.remove("Tesla");
 
-        // Change BMW to Audi
         int index = cars.indexOf("BMW");
         if (index != -1) {
             cars.set(index, "Audi");
         }
 
-        // Print modified list
         System.out.println("MODIFIED LIST");
         for (String car : cars) {
             System.out.println(car);
         }
 
-        // Sort the list
         Collections.sort(cars);
 
-        // Print sorted list
         System.out.println("SORTED LIST");
         for (String car : cars) {
             System.out.println(car);
-        }
+
+        }     ///("Pahoittelut viivästyksestä");
+
     }
 }
