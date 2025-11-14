f454f462059fe3753cb4fbce39d3d3300506ac22
AleksiOikarinen <aleksi23007@student.hamk.fi>
2025-11-14 08:46:03 +0200

    save

--- Olio-ohjelmointi/Teht5/Kala.java
+++ Olio-ohjelmointi/Teht5/Kala.java
@@ -1,5 +1,59 @@
 package Teht5;
 
 public class Kala {
+    private int id;
+    private String laji;
+    private float paino;
 
+    public int getId() {
+        return id;
+    }
+
+    // Metodi setId on ylikuormitettu
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setId(String id) {
+        try {
+            this.id = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            // jätetään virhe huomioimatta
+        }
+    }
+
+    public String getLaji() {
+        return laji;
+    }
+
+    public void setLaji(String laji) {
+        this.laji = laji;
+    }
+
+    public float getPaino() {
+        return paino;
+    }
+
+    // Metodi setPaino on ylikuormitettu
+    public boolean setPaino(float paino) {
+        if (paino >= 0 && paino < 50000) {
+            this.paino = paino;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setPaino(String paino) {
+        try {
+            return this.setPaino(Float.parseFloat(paino));
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    // Kun pääohjelmassa tulostetaan (System.out.println(olioviittaus))
+    // Tulostetaan toString-metodin palauttama arvo
+    public String toString() {
+        return this.id + ": " + this.laji + " (" + this.paino + ")";
+    }
 }
--- Olio-ohjelmointi/Teht5/KalaTaulu.java
+++ Olio-ohjelmointi/Teht5/KalaTaulu.java
@@ -1,5 +1,49 @@
 package Teht5;
 
+import java.util.Scanner;
+
 public class KalaTaulu {
+
+	public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Kala[] taulu = new Kala[3];
+
+        for(int i= 0; i < taulu.length; i++) {
+            taulu[i] =new Kala();
+            System.out.println("Anna kalan tiedot" + (i + 1) + ":");
+            lueKalanTiedot(taulu[i], input);
+            System.out.println();
+        }
+
+        tulostaKalat(taulu);
+
+        input.close();
+    }
+
+    private static void lueKalanTiedot(Kala kala, Scanner input) {
+
+        System.out.print("Anna kalan id");
+        String id =input.nextLine();
+        kala.setId(id);
+    
+
+        System.out.print("Anna kalan laji");
+        String laji =input.nextLine();
+        kala.setLaji(laji);
+
+        System.out.print("Anna kalan paino");
+        String paino =input.nextLine();
+        if (!kala.setPaino(paino)) {
+            System.out.println("Virheellinen paino");
+        }
+    }
+
+
+	private static void tulostaKalat(Kala[] taulu) {
+		for (int i=0;i<taulu.length;i++) {
+			System.out.println(taulu[i]);
+		}
+	}
 
 }
