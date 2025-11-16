2ece22e161d5e068b4b81eb3e23c9c77e2d9f1f4
AleksiOikarinen <aleksi23007@student.hamk.fi>
2025-11-16 14:18:33 +0200

    Python

---
+++ Python/printer_app.py
@@ -0,0 +1,24 @@
+# Python printer APp
+creator = "Aleksi"
+number1 = 7
+number2 = 3
+
+multiplication = number1 * number2
+addition = number1 + number2
+division = number1 / number2
+subtraction = number1 - number2
+
+
+
+print("Printer App")
+print("This app is made by:", creator)
+
+
+
+print("The value of number1 is", number1)
+print("The value of number2 is", number2)
+
+print(number1, "*", number2, "=", multiplication)
+print(number1, "+", number2, "=", addition)
+print(number1, "/", number2, "=", division)
+print(number1, "-", number2, "=", subtraction)
