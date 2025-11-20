283be7fccff0b3844211c5a1ea650c1ccfb27e0e
AleksiOikarinen <aleksi23007@student.hamk.fi>
2025-11-20 09:48:27 +0200

    Update KalaOhjelma.java

--- Olio-ohjelmointi/Teht6/KalaOhjelma.java
+++ Olio-ohjelmointi/Teht6/KalaOhjelma.java
@@ -9,27 +9,27 @@ public class KalaOhjelma {
     
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        ArrayList<Kala> lista = new ArrayList<>();
+        ArrayList<Kala> lista = new ArrayList<>();                //Luodaan lista (tämä toistuu kunnes muin sanotaan)
 
         do {
-            Kala k = new Kala();
-            KysyKalanTiedot(k);
-            lista.add(k);
-        } while (kysy("Lisätäänkö uusi kala? (k/e)").equalsIgnoreCase("k"));
+            Kala k = new Kala();              //Luodaan uusi kala nimeltä k
+            KysyKalanTiedot(k);                      //kysytään kalan tiedot
+            lista.add(k);                        //Lisataan kala listaan
+        } while (kysy("Lisätäänkö uusi kala? (k/e)").equalsIgnoreCase("k"));        //Kysytään halutaanko lisätä uusi kala
         tulostaKalat(lista);
     }
-    public static void KysyKalanTiedot(Kala k) {
+    public static void KysyKalanTiedot(Kala k) {        //Kalan parametrit nappaimistolta
         k.setId(kysy("Anna kalan id: "));
         k.setLaji(kysy("Anna kalan laji: "));
-        if (!k.setPaino(kysy("Anna kalan paino: ")))
+        if (!k.setPaino(kysy("Anna kalan paino: ")))           //Jos ei anneta painoa niin tulos 0
         System.out.println("Asennetaan paino 0.");
     }
-    public static String kysy(String teksti) {
+    public static String kysy(String teksti) {                      //tulostaa vastaukset näppäimistöltä
         System.out.print(teksti);
         return sc.nextLine().trim();
     }
     public static void tulostaKalat(ArrayList<Kala> lista) {
-        System.out.println("--Kaikki kalat--");
-        lista.forEach(System.out::println);
+        System.out.println("--Kaikki kalat--");                //tulostetaan peini titteli
+        lista.forEach(System.out::println);                //tulostaa koko listan
     }
 }
