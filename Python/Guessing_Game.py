737fb21cd4108bd4d13933998c3afe7faf9aec24
AleksiOikarinen <aleksi23007@student.hamk.fi>
2025-11-25 15:02:37 +0200

    Create Guessing_Game.py

---
+++ Python/Guessing_Game.py
@@ -0,0 +1,18 @@
+#correctName = "Aleksi"
+
+#guess = input("Guess the name:")
+
+#if guess == correctName:
+#    print("Congratulations!")
+
+#else:
+#    print ("Wrong answer!")
+
+correctName = "Aleksi"
+
+guess = ""
+
+while guess != correctName:
+    guess = input("Guess the name:")
+
+print("Congratulations!")
